13f82a88361ed55f01a40fb2be991f2abcae47c4
Kiahna Carmack <Kiahna.carmack@gmail.com>
2022-04-11 15:44:33 -0700

    final commit

---
+++ models/Tag.js
@@ -0,0 +1,31 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Tag extends Model {}
+
+Tag.init(
+ {
+   tag_name: {
+     type: DataTypes.STRING,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'tag',
+  }
+);
+
+module.exports = Tag;
+
+/*foo({
+  colName:{
+    type:string
+  }
+},{
+  option1:true,
+  oprtion2:false
+})*/
